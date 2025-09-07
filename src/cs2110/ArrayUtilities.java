52cfcf83958f06efe72c788fbae9862db3c1af9f
Henry Ji <hj465@cornell.edu>
2025-09-06 20:18:35 -0400

    initial commit

---
+++ src/cs2110/ArrayUtilities.java
@@ -0,0 +1,28 @@
+package cs2110;
+
+/**
+ * Contains utility methods for copying an array range to another array range (potentially within
+ * the same array).
+ */
+public class ArrayUtilities {
+
+    // TODO 1: Write JavaDoc specifications for this method based on the description of its behavior
+    //  in the assignment handout. This documentation should include a list of all scenarios that
+    //  will cause the method to return `false`.
+    static boolean copyRange(int[] src, int srcStart, int[] dst, int dstStart, int length) {
+        // TODO 4: Complete the definition of this method. You may not call any methods outside of
+        //  the `ArrayUtilities` class, and you must document the invariants of any loop(s) with a
+        //  comment.
+        throw new UnsupportedOperationException();
+    }
+
+    // TODO 6a: Write JavaDoc specifications for this method based on the description of its behavior
+    //  in the assignment handout.
+    static boolean copy2DRange(int[][] src, int srcI, int srcJ, int[][] dst, int dstI, int dstJ,
+            int height, int width) {
+        // TODO 6b: Complete the definition of this method. You may not call any methods outside of
+        //  the `ArrayUtilities` class, and you must document the invariants of any loop(s) with a
+        //  comment.
+        throw new UnsupportedOperationException();
+    }
+}
